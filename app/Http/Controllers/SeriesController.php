7dd0fd26e09f3ebf92b6290002127e21217b0f65
Augusto Zeponi <augustozeponi@hotmail.com>
2023-11-02 19:47:57 -0300

    Criando rotas e armazenado series no sqlite

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy',
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +18,12 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        Serie::create($request->all());
+
+        return redirect('/series');
     }
 }
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Nova Série">
-    <form action="" method="post">
+    <form action="salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name="nome" class="form-control">
